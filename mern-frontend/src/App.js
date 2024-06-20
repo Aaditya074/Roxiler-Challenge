73f53394b2b7390f574437c76a2a71c96c37922d
Aaditya Gupta <75359507+Aaditya074@users.noreply.github.com>
2024-06-20 13:28:48 +0530

    Add files via upload

---
+++ mern-frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Dashboard from './components/DashBoard';
+
+function App() {
+  return (
+    <Dashboard/>
+  );
+}
+
+export default App;
